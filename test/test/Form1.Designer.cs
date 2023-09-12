972906e6a452813c891b6d12ca81dd8bc7f5c9ec
15HuyenNguyen <144752540+15HuyenNguyen@users.noreply.github.com>
2023-09-12 11:32:35 +0700

    Add files via upload

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace test
+{
+    partial class btnNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSoPT = new System.Windows.Forms.TextBox();
+            this.btnTang = new System.Windows.Forms.Button();
+            this.btnGiam = new System.Windows.Forms.Button();
+            this.tbMang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNhapMang = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSoDau = new System.Windows.Forms.TextBox();
+            this.tbSoCuoi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbKetQua = new System.Windows.Forms.TextBox();
+            this.msg = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rbChon = new System.Windows.Forms.RadioButton();
+            this.rbChen = new System.Windows.Forms.RadioButton();
+            this.rbNoiBot = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số phần tử";
+            // 
+            // tbSoPT
+            // 
+            this.tbSoPT.Location = new System.Drawing.Point(167, 66);
+            this.tbSoPT.Name = "tbSoPT";
+            this.tbSoPT.Size = new System.Drawing.Size(67, 22);
+            this.tbSoPT.TabIndex = 16;
+            // 
+            // btnTang
+            // 
+            this.btnTang.Location = new System.Drawing.Point(210, 207);
+            this.btnTang.Name = "btnTang";
+            this.btnTang.Size = new System.Drawing.Size(132, 32);
+            this.btnTang.TabIndex = 2;
+            this.btnTang.Text = "Sắp xếp tăng";
+            this.btnTang.UseVisualStyleBackColor = true;
+            this.btnTang.Click += new System.EventHandler(this.btnTang_Click);
+            // 
+            // btnGiam
+            // 
+            this.btnGiam.Location = new System.Drawing.Point(348, 207);
+            this.btnGiam.Name = "btnGiam";
+            this.btnGiam.Size = new System.Drawing.Size(132, 32);
+            this.btnGiam.TabIndex = 3;
+            this.btnGiam.Text = "Sắp xếp giảm";
+            this.btnGiam.UseVisualStyleBackColor = true;
+            this.btnGiam.Click += new System.EventHandler(this.btnGiam_Click);
+            // 
+            // tbMang
+            // 
+            this.tbMang.Location = new System.Drawing.Point(165, 260);
+            this.tbMang.Name = "tbMang";
+            this.tbMang.Size = new System.Drawing.Size(346, 22);
+            this.tbMang.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mảng";
+            // 
+            // btnNhapMang
+            // 
+            this.btnNhapMang.Location = new System.Drawing.Point(325, 113);
+            this.btnNhapMang.Name = "btnNhapMang";
+            this.btnNhapMang.Size = new System.Drawing.Size(92, 32);
+            this.btnNhapMang.TabIndex = 6;
+            this.btnNhapMang.Text = "Nhập mảng";
+            this.btnNhapMang.UseVisualStyleBackColor = true;
+            this.btnNhapMang.Click += new System.EventHandler(this.btnNhapMang_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(227, 113);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(92, 32);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Số đầu";
+            // 
+            // tbSoDau
+            // 
+            this.tbSoDau.Location = new System.Drawing.Point(313, 66);
+            this.tbSoDau.Name = "tbSoDau";
+            this.tbSoDau.Size = new System.Drawing.Size(59, 22);
+            this.tbSoDau.TabIndex = 10;
+            // 
+            // tbSoCuoi
+            // 
+            this.tbSoCuoi.Location = new System.Drawing.Point(452, 66);
+            this.tbSoCuoi.Name = "tbSoCuoi";
+            this.tbSoCuoi.Size = new System.Drawing.Size(59, 22);
+            this.tbSoCuoi.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Số cuối";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Kết quả";
+            // 
+            // tbKetQua
+            // 
+            this.tbKetQua.Location = new System.Drawing.Point(165, 301);
+            this.tbKetQua.Name = "tbKetQua";
+            this.tbKetQua.Size = new System.Drawing.Size(346, 22);
+            this.tbKetQua.TabIndex = 14;
+            // 
+            // msg
+            // 
+            this.msg.Enabled = false;
+            this.msg.Location = new System.Drawing.Point(85, 404);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(235, 22);
+            this.msg.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 18;
+            // 
+            // rbChon
+            // 
+            this.rbChon.AutoSize = true;
+            this.rbChon.Location = new System.Drawing.Point(167, 170);
+            this.rbChon.Name = "rbChon";
+            this.rbChon.Size = new System.Drawing.Size(62, 21);
+            this.rbChon.TabIndex = 21;
+            this.rbChon.TabStop = true;
+            this.rbChon.Text = "Chọn";
+            this.rbChon.UseVisualStyleBackColor = true;
+            // 
+            // rbChen
+            // 
+            this.rbChen.AutoSize = true;
+            this.rbChen.Location = new System.Drawing.Point(294, 170);
+            this.rbChen.Name = "rbChen";
+            this.rbChen.Size = new System.Drawing.Size(62, 21);
+            this.rbChen.TabIndex = 22;
+            this.rbChen.TabStop = true;
+            this.rbChen.Text = "Chèn";
+            this.rbChen.UseVisualStyleBackColor = true;
+            // 
+            // rbNoiBot
+            // 
+            this.rbNoiBot.AutoSize = true;
+            this.rbNoiBot.Location = new System.Drawing.Point(418, 170);
+            this.rbNoiBot.Name = "rbNoiBot";
+            this.rbNoiBot.Size = new System.Drawing.Size(74, 21);
+            this.rbNoiBot.TabIndex = 23;
+            this.rbNoiBot.TabStop = true;
+            this.rbNoiBot.Text = "Nổi bọt";
+            this.rbNoiBot.UseVisualStyleBackColor = true;
+            // 
+            // btnNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbNoiBot);
+            this.Controls.Add(this.rbChen);
+            this.Controls.Add(this.rbChon);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbKetQua);
+            this.Controls.Add(this.tbSoCuoi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbSoDau);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnNhapMang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMang);
+            this.Controls.Add(this.btnGiam);
+            this.Controls.Add(this.btnTang);
+            this.Controls.Add(this.tbSoPT);
+            this.Controls.Add(this.label1);
+            this.Name = "btnNhap";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSoPT;
+        private System.Windows.Forms.Button btnTang;
+        private System.Windows.Forms.Button btnGiam;
+        private System.Windows.Forms.TextBox tbMang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNhapMang;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSoDau;
+        private System.Windows.Forms.TextBox tbSoCuoi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbKetQua;
+        private System.Windows.Forms.TextBox msg;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rbChon;
+        private System.Windows.Forms.RadioButton rbChen;
+        private System.Windows.Forms.RadioButton rbNoiBot;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class btnNhap : Form
+    {
+        private Random random;
+        Stopwatch stopwatch = new Stopwatch();
+        public btnNhap()
+        {
+            InitializeComponent();
+            random = new Random();
+
+        }
+        int n = 0, i, tam;
+        private List<int> randomArray;
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            tbSoPT.ResetText();
+            tbMang.Clear();
+            tbSoCuoi.ResetText();
+            tbSoDau.ResetText();
+            tbKetQua.ResetText();
+            msg.ResetText();
+            n = 0;
+        }
+
+        private void btnNhapMang_Click(object sender, EventArgs e)
+        {
+            int size = int.Parse(tbSoPT.Text);
+            int min = int.Parse(tbSoDau.Text);
+            int max = int.Parse(tbSoCuoi.Text);
+            randomArray = new List<int>();
+            // Tạo mảng ngẫu nhiên
+            for (int i = 0; i < size; i++)
+            {
+                randomArray.Add(random.Next(min, max));
+            }
+            // Hiển thị mảng ngẫu nhiên
+            tbMang.Text = string.Join(" ", randomArray);
+        }
+
+        private void btnGiam_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            stopwatch.Start();
+            n = randomArray.Count;
+            if (randomArray == null || randomArray.Count == 0)
+            {
+                MessageBox.Show("Vui lòng tạo mảng trước khi sắp xếp.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (rbNoiBot.Checked)
+            {
+                for (i = 0; i < n - 1; i++)
+                    for (int j = i + 1; j < n; j++)
+                        if (randomArray[i] < randomArray[j])
+                        {
+                            tam = randomArray[i];
+                            randomArray[i] = randomArray[j];
+                            randomArray[j] = tam;
+                        }
+                // Hiển thị mảng đã sắp xếp
+                tbKetQua.Text = string.Join(" ", randomArray);
+                stopwatch.Stop();
+                msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+            }
+            else
+            if (rbChon.Checked)
+            {
+                for (i = 0; i < n - 1; i++)
+                {
+                    int minIndex = i;
+                    // Tìm phần tử nhỏ nhất trong mảng chưa sắp xếp
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (randomArray[j] > randomArray[minIndex])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                    if (i != minIndex)
+                    {
+                        int temp = randomArray[i];
+                        randomArray[i] = randomArray[minIndex];
+                        randomArray[minIndex] = temp;
+                    }
+                    tbKetQua.Text = string.Join(" ", randomArray);
+                    stopwatch.Stop();
+                    msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+                }
+            }
+            else if (rbChen.Checked)
+            {
+                for (i = 1; i < n; i++)
+                {
+                    int key = randomArray[i];
+                    int j = i - 1;
+                    while (j >= 0 && randomArray[j] < key)
+                    {
+                        randomArray[j + 1] = randomArray[j];
+                        j--;
+                    }
+                    randomArray[j + 1] = key;
+                }
+                    tbKetQua.Text = string.Join(" ", randomArray);
+                    stopwatch.Stop();
+                    msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+                }
+            }
+
+        private void btnTang_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            stopwatch.Start();
+            if (randomArray == null || randomArray.Count == 0)
+            {
+                MessageBox.Show("Vui lòng tạo mảng trước khi sắp xếp.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int n = randomArray.Count;
+            if (rbNoiBot.Checked)
+            {
+                for (i = 0; i < n - 1; i++)
+                    for (int j = i + 1; j < n; j++)
+                        //for (int j = 0; j < n - i - 1; j++)
+                        if (randomArray[i] > randomArray[j])
+                        {
+                            tam = randomArray[i];
+                            randomArray[i] = randomArray[j];
+                            randomArray[j] = tam;
+                        }
+
+                // Hiển thị mảng đã sắp xếp
+                tbKetQua.Text = string.Join(" ", randomArray);
+                stopwatch.Stop();
+                msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+            }
+            else if (rbChon.Checked)
+            {
+                tbKetQua.ResetText();
+                for (i = 0; i < n - 1; i++)
+                {
+                    int minIndex = i;
+                    // Tìm phần tử nhỏ nhất trong mảng chưa sắp xếp
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (randomArray[j] < randomArray[minIndex])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                    if (i != minIndex)
+                    {
+                        int temp = randomArray[i];
+                        randomArray[i] = randomArray[minIndex];
+                        randomArray[minIndex] = temp;
+                    }
+                    tbKetQua.Text = string.Join(" ", randomArray);
+                    stopwatch.Stop();
+                    msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+                }
+            }
+            else if (rbChen.Checked)
+            {
+                for (i = 1; i < n; i++)
+                {
+                    int key = randomArray[i];
+                    int j = i - 1;
+                    while (j >= 0 && randomArray[j] > key)
+                    {
+                        randomArray[j + 1] = randomArray[j];
+                        j--;
+                    }
+                    randomArray[j + 1] = key;
+                }
+                tbKetQua.Text = string.Join(" ", randomArray);
+                stopwatch.Stop();
+                msg.Text = "Thời gian thực hiện:  " + stopwatch.Elapsed.TotalMilliseconds.ToString("0.######") + " ms";
+            }
+        }
+    }
+}
+
+
+
+
+
+
+/*private void btnXoa_Click(object sender, EventArgs e)
+        {
+            KhungNhap.ResetText();
+            KhungXuat.Clear();
+            KhungXuat.ResetText();
+            n = 0;
+            s = " ";
+        }
+
+        private void btnNhapMang_Click(object sender, EventArgs e)
+        {
+            s = KhungNhap.Text;
+            i = s.LastIndexOf(" ");
+            while (i != -1)
+            {
+                s1 = s.Substring(i);
+                s = s.Substring(0, i);
+                a[n] = Convert.ToInt32(s1);
+                n++;
+                i = s.LastIndexOf(" ");
+            }
+            a[n] = Convert.ToInt32(s);
+            s = " ";
+            for (i = n; i >= 0; i--)
+                s = s + " " + a[i].ToString();
+            KhungXuat.Text = s.Trim();
+        }
+
+        private void btnGiam_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < n; i++)
+                for (int j = i + 1; j <= n; j++)
+                    if (a[i] < a[j])
+                    {
+                        tam = a[i];
+                        a[i] = a[j];
+                        a[j] = tam;
+                    }
+            s = " ";
+            for (i = 0; i <= n; i++)
+                s = s + " " + a[i].ToString();
+            KhungXuat.Text = s.Trim();
+        }
+
+
+        private void btnTang_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < n; i++)
+                for (int j = i + 1; j <= n; j++)
+                    if (a[i] > a[j])
+                    {
+                        tam = a[i];
+                        a[i] = a[j];
+                        a[j] = tam;
+                    }
+            s = " ";
+            for (i = 0; i <= n; i++)
+                s = s + " " + a[i].ToString();
+            KhungXuat.Text = s.Trim();
+
+        }
+    }*/
